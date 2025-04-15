2365e18a7ad970614c86bf6deaf5bdc5ca17a500
Garoev Aleksandr <garoevae@student.bmstu.ru>
2025-04-15 14:43:43 +0300

    little fix for map

--- internal/pkg/helpers/errorStatus.go
+++ internal/pkg/helpers/errorStatus.go
@@ -8,27 +8,21 @@ import (
 	"github.com/go-park-mail-ru/2025_1_Return_Zero/internal/pkg/track"
 )
 
+var mapErrorStatus = map[error]int{
+	track.ErrTrackNotFound:                http.StatusNotFound,
+	track.ErrStreamNotFound:               http.StatusNotFound,
+	track.ErrFailedToUpdateStreamDuration: http.StatusInternalServerError,
+	track.ErrStreamPermissionDenied:       http.StatusForbidden,
+	artist.ErrArtistNotFound:              http.StatusNotFound,
+	album.ErrAlbumNotFound:                http.StatusNotFound,
+	ErrInvalidOffset:                      http.StatusBadRequest,
+	ErrInvalidLimit:                       http.StatusBadRequest,
+}
+
 func ErrorStatus(err error) int {
-	switch err {
-	case track.ErrTrackNotFound:
-		return http.StatusNotFound
-	case track.ErrStreamNotFound:
-		return http.StatusNotFound
-	case track.ErrFailedToUpdateStreamDuration:
-		return http.StatusInternalServerError
-	case track.ErrStreamPermissionDenied:
-		return http.StatusForbidden
-	case artist.ErrArtistNotFound:
-		return http.StatusNotFound
-	case album.ErrAlbumNotFound:
-		return http.StatusNotFound
-	case artist.ErrArtistNotFound:
-		return http.StatusNotFound
-	case ErrInvalidOffset:
-		return http.StatusBadRequest
-	case ErrInvalidLimit:
-		return http.StatusBadRequest
-	default:
+	status, exists := mapErrorStatus[err]
+	if !exists {
 		return http.StatusInternalServerError
 	}
+	return status
 }
