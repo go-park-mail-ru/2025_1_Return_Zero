cfda6d4d74a7ee01d5a4bdbfc81fa0f2e7d88a02
Garoev Aleksandr <garoevae@student.bmstu.ru>
2025-05-05 12:23:24 +0300

    new tracks, albums and artists

--- db/migrations/002_data.sql
+++ db/migrations/002_data.sql
@@ -7,7 +7,11 @@ INSERT INTO artist (title, thumbnail_url, description) VALUES
 	('RADWIMPS', 'https://returnzeroimages.fra1.digitaloceanspaces.com/artists/radwimps.jpg', 'RADWIMPS is a Japanese artist'),
 	('Official HIGE DANdism', 'https://returnzeroimages.fra1.digitaloceanspaces.com/artists/officialhigedandism.jpg', 'Official HIGE DANdism is a Japanese artist'),
     ('Toaka', 'https://returnzeroimages.fra1.digitaloceanspaces.com/artists/toaka.jpg', 'Toaka is a Japanese artist'),
-    ('Ayase', 'https://returnzeroimages.fra1.digitaloceanspaces.com/artists/ayase.jpg', 'Ayase is a Japanese artist');
+    ('Ayase', 'https://returnzeroimages.fra1.digitaloceanspaces.com/artists/ayase.jpg', 'Ayase is a Japanese artist'),
+    ('Eminem', 'https://returnzeroimages.fra1.digitaloceanspaces.com/artists/eminem.jpg', 'Cool man'),
+    ('The Cab', 'https://returnzeroimages.fra1.digitaloceanspaces.com/artists/thecab.jpg', 'Angel'),
+    ('Sergey Eybog', 'https://returnzeroimages.fra1.digitaloceanspaces.com/artists/sergeyeybog.jpg', 'Free musician'),
+    ('Katalepsy', 'https://returnzeroimages.fra1.digitaloceanspaces.com/artists/katalepsy.jpg', 'Slamming brutal death metal band from Russia');
 
 INSERT INTO album (title, thumbnail_url, release_date, type) VALUES
 	('Anticyclone', 'https://returnzeroimages.fra1.digitaloceanspaces.com/albums/anticyclone.jpg', '2023-01-01', 'album'),
@@ -15,21 +19,29 @@ INSERT INTO album (title, thumbnail_url, release_date, type) VALUES
 	('BOOTLEG', 'https://returnzeroimages.fra1.digitaloceanspaces.com/albums/bootleg.jpg', '2022-01-01', 'ep'),
 	('Your Name.', 'https://returnzeroimages.fra1.digitaloceanspaces.com/albums/yourname.jpg', '2021-01-01', 'album'),
     ('Official HIGE DANdism', 'https://returnzeroimages.fra1.digitaloceanspaces.com/albums/officialhigedandism.jpg', '2020-01-01', 'album'),
-    ('Ghost City Tokyo', 'https://returnzeroimages.fra1.digitaloceanspaces.com/albums/ghostcitytokyo.jpg', '2024-01-01', 'album');
+    ('Ghost City Tokyo', 'https://returnzeroimages.fra1.digitaloceanspaces.com/albums/ghostcitytokyo.jpg', '2024-01-01', 'album'),
+    ('The Eminem Show', 'https://returnzeroimages.fra1.digitaloceanspaces.com/albums/theeminemshow.jpg', '2002-01-01', 'album'),
+    ('Symphony Soldier', 'https://returnzeroimages.fra1.digitaloceanspaces.com/albums/symphonysoldier.jpg', '2015-01-01', 'single'),
+    ('Everlasting Summer', 'https://returnzeroimages.fra1.digitaloceanspaces.com/albums/everlastingsummer.jpg', '2016-01-01', 'album'),
+    ('Music Brings Injures', 'https://returnzeroimages.fra1.digitaloceanspaces.com/albums/musicbringsinjures.webp', '2024-01-01', 'album'),
+    ('Triumph Of Evilution', 'https://returnzeroimages.fra1.digitaloceanspaces.com/albums/triumphofevilution.webp', '2024-01-01', 'ep');
 
 INSERT INTO genre (name) VALUES
 	('J-Pop'),
 	('Rock'),
 	('Electronic'),
 	('Pop'),
-	('Hip-Hop');
+	('Hip-Hop'),
+    ('OST');
 
 INSERT INTO genre_album (genre_id, album_id) VALUES
 	((SELECT id FROM genre WHERE name = 'J-Pop'), (SELECT id FROM album WHERE title = 'Anticyclone')),
-	((SELECT id FROM genre WHERE name = 'Rock'), (SELECT id FROM album WHERE title = 'Anticyclone')),
 	((SELECT id FROM genre WHERE name = 'Electronic'), (SELECT id FROM album WHERE title = 'THE BOOK')),
 	((SELECT id FROM genre WHERE name = 'Pop'), (SELECT id FROM album WHERE title = 'BOOTLEG')),
-	((SELECT id FROM genre WHERE name = 'Hip-Hop'), (SELECT id FROM album WHERE title = 'Your Name.'));
+	((SELECT id FROM genre WHERE name = 'OST'), (SELECT id FROM album WHERE title = 'Your Name.')),
+    ((SELECT id FROM genre WHERE name = 'Hip-Hop'), (SELECT id FROM album WHERE title = 'The Eminem Show')),
+    ((SELECT id FROM genre WHERE name = 'Rock'), (SELECT id FROM album WHERE title = 'Symphony Soldier')),
+    ((SELECT id FROM genre WHERE name = 'OST'), (SELECT id FROM album WHERE title = 'Everlasting Summer'));
 
 INSERT INTO track (title, album_id, duration, thumbnail_url, file_url, position) VALUES
     ('Lagtrain', (SELECT id FROM album WHERE title = 'Anticyclone'), 252, 'https://returnzeroimages.fra1.digitaloceanspaces.com/tracks/lagtrain.jpg', 'lagtrain.mp3', 1),
@@ -51,7 +63,22 @@ INSERT INTO track (title, album_id, duration, thumbnail_url, file_url, position)
     ('Shinigami', (SELECT id FROM album WHERE title = 'BOOTLEG'), 181, 'https://returnzeroimages.fra1.digitaloceanspaces.com/tracks/shinigami.jpg', 'shinigami.mp3', 4),
     ('Gunjo', (SELECT id FROM album WHERE title = 'THE BOOK'), 248, 'https://returnzeroimages.fra1.digitaloceanspaces.com/tracks/gunjo.jpg', 'gunjo.mp3', 4),
     ('Tabun', (SELECT id FROM album WHERE title = 'THE BOOK'), 263, 'https://returnzeroimages.fra1.digitaloceanspaces.com/tracks/tabun.jpg', 'tabun.mp3', 5),
-    ('Ghost City Tokyo', (SELECT id FROM album WHERE title = 'Ghost City Tokyo'), 204, 'https://returnzeroimages.fra1.digitaloceanspaces.com/tracks/ghostcitytokyo.jpg', 'ghostcitytokyo.mp3', 1);
+    ('Ghost City Tokyo', (SELECT id FROM album WHERE title = 'Ghost City Tokyo'), 204, 'https://returnzeroimages.fra1.digitaloceanspaces.com/tracks/ghostcitytokyo.jpg', 'ghostcitytokyo.mp3', 1),
+    ('Without Me', (SELECT id FROM album WHERE title = 'The Eminem Show'), 289, 'https://returnzeroimages.fra1.digitaloceanspaces.com/tracks/theeminemshow.jpg', 'withoutme.mp3', 1),
+    ('Sing For The Moment', (SELECT id FROM album WHERE title = 'The Eminem Show'), 340, 'https://returnzeroimages.fra1.digitaloceanspaces.com/tracks/theeminemshow.jpg', 'singforthemoment.mp3', 2),
+    ('Angel With A Shotgun', (SELECT id FROM album WHERE title = 'Symphony Soldier'), 201, 'https://returnzeroimages.fra1.digitaloceanspaces.com/tracks/symphonysoldier.jpg', 'angelwithashotgun.mp3', 1),
+    ('Timid Girl', (SELECT id FROM album WHERE title = 'Everlasting Summer'), 82, 'https://returnzeroimages.fra1.digitaloceanspaces.com/tracks/everlastingsummer.jpg', 'timidgirl.mp3', 1),
+    ('Let`s be friends', (SELECT id FROM album WHERE title = 'Everlasting Summer'), 123, 'https://returnzeroimages.fra1.digitaloceanspaces.com/tracks/everlastingsummer.jpg', 'letbefriends.mp3', 2),
+    ('Gialo', (SELECT id FROM album WHERE title = 'Music Brings Injures'), 212, 'https://returnzeroimages.fra1.digitaloceanspaces.com/tracks/musicbringsinjures.webp', 'gialo.mp3', 1),
+    ('Sluggish Cranial Grinding', (SELECT id FROM album WHERE title = 'Music Brings Injures'), 224, 'https://returnzeroimages.fra1.digitaloceanspaces.com/tracks/musicbringsinjures.webp', 'sluggishcranialgrinding.mp3', 2),
+    ('Rabid', (SELECT id FROM album WHERE title = 'Music Brings Injures'), 137, 'https://returnzeroimages.fra1.digitaloceanspaces.com/tracks/musicbringsinjures.webp', 'rabid.mp3', 3),
+    ('Necroviolated To Liquid', (SELECT id FROM album WHERE title = 'Music Brings Injures'), 226, 'https://returnzeroimages.fra1.digitaloceanspaces.com/tracks/musicbringsinjures.webp', 'necroviolatedtoliquid.mp3', 4),
+    ('ConsumingTheAbyss', (SELECT id FROM album WHERE title = 'Music Brings Injures'), 223, 'https://returnzeroimages.fra1.digitaloceanspaces.com/tracks/musicbringsinjures.webp', 'consumingtheabyss.mp3', 5),
+    ('S.O.D.', (SELECT id FROM album WHERE title = 'Music Brings Injures'), 265, 'https://returnzeroimages.fra1.digitaloceanspaces.com/tracks/musicbringsinjures.webp', 'sod.mp3', 6),
+    ('Post-Apocalyptic Segregation', (SELECT id FROM album WHERE title = 'Triumph Of Evilution'), 251, 'https://returnzeroimages.fra1.digitaloceanspaces.com/albums/triumphofevilution.webp', 'postapocalypticsegregation.mp3', 1),
+    ('Carpet Wounding', (SELECT id FROM album WHERE title = 'Triumph Of Evilution'), 255, 'https://returnzeroimages.fra1.digitaloceanspaces.com/albums/triumphofevilution.webp', 'carpetwounding.mp3', 2),
+    ('H. Tearing Sinew', (SELECT id FROM album WHERE title = 'Triumph Of Evilution'), 285, 'https://returnzeroimages.fra1.digitaloceanspaces.com/albums/triumphofevilution.webp', 'htearingsinew.mp3', 3),
+    ('Number Of Death', (SELECT id FROM album WHERE title = 'Triumph Of Evilution'), 244, 'https://returnzeroimages.fra1.digitaloceanspaces.com/albums/triumphofevilution.webp', 'numberofdeath.mp3', 4);
 
 
 INSERT INTO genre_track (genre_id, track_id) VALUES
@@ -59,7 +86,10 @@ INSERT INTO genre_track (genre_id, track_id) VALUES
     ((SELECT id FROM genre WHERE name = 'Rock'), (SELECT id FROM track WHERE title = 'Lagtrain')),
     ((SELECT id FROM genre WHERE name = 'Electronic'), (SELECT id FROM track WHERE title = 'Lost Umbrella')),
     ((SELECT id FROM genre WHERE name = 'Pop'), (SELECT id FROM track WHERE title = 'Racing Into The Night')),
-    ((SELECT id FROM genre WHERE name = 'Hip-Hop'), (SELECT id FROM track WHERE title = 'Idol'));
+    ((SELECT id FROM genre WHERE name = 'Hip-Hop'), (SELECT id FROM track WHERE title = 'Idol')),
+    ((SELECT id FROM genre WHERE name = 'OST'), (SELECT id FROM track WHERE title = 'Timid Girl')),
+    ((SELECT id FROM genre WHERE name = 'OST'), (SELECT id FROM track WHERE title = 'Let`s be friends')),
+    ((SELECT id FROM genre WHERE name = 'Rock'), (SELECT id FROM track WHERE title = 'Angel With A Shotgun'));
 
 INSERT INTO track_artist (track_id, artist_id, role) VALUES
     ((SELECT id FROM track WHERE title = 'Lagtrain'), (SELECT id FROM artist WHERE title = 'Inabakumori'), 'main'),
@@ -82,7 +112,23 @@ INSERT INTO track_artist (track_id, artist_id, role) VALUES
     ((SELECT id FROM track WHERE title = 'Shinigami'), (SELECT id FROM artist WHERE title = 'Kenshi Yonezu'), 'main'),
     ((SELECT id FROM track WHERE title = 'Gunjo'), (SELECT id FROM artist WHERE title = 'YOASOBI'), 'main'),
     ((SELECT id FROM track WHERE title = 'Tabun'), (SELECT id FROM artist WHERE title = 'YOASOBI'), 'main'),
-    ((SELECT id FROM track WHERE title = 'Ghost City Tokyo'), (SELECT id FROM artist WHERE title = 'Ayase'), 'main');
+    ((SELECT id FROM track WHERE title = 'Ghost City Tokyo'), (SELECT id FROM artist WHERE title = 'Ayase'), 'main'),
+    ((SELECT id FROM track WHERE title = 'Without Me'), (SELECT id FROM artist WHERE title = 'Eminem'), 'main'),
+    ((SELECT id FROM track WHERE title = 'Sing For The Moment'), (SELECT id FROM artist WHERE title = 'Eminem'), 'main'),
+    ((SELECT id FROM track WHERE title = 'Angel With A Shotgun'), (SELECT id FROM artist WHERE title = 'The Cab'), 'main'),
+    ((SELECT id FROM track WHERE title = 'Timid Girl'), (SELECT id FROM artist WHERE title = 'Sergey Eybog'), 'main'),
+    ((SELECT id FROM track WHERE title = 'Let`s be friends'), (SELECT id FROM artist WHERE title = 'Sergey Eybog'), 'main'),
+    ((SELECT id FROM track WHERE title = 'Gialo'), (SELECT id FROM artist WHERE title = 'Katalepsy'), 'main'),
+    ((SELECT id FROM track WHERE title = 'Sluggish Cranial Grinding'), (SELECT id FROM artist WHERE title = 'Katalepsy'), 'main'),
+    ((SELECT id FROM track WHERE title = 'Rabid'), (SELECT id FROM artist WHERE title = 'Katalepsy'), 'main'),
+    ((SELECT id FROM track WHERE title = 'Necroviolated To Liquid'), (SELECT id FROM artist WHERE title = 'Katalepsy'), 'main'),
+    ((SELECT id FROM track WHERE title = 'ConsumingTheAbyss'), (SELECT id FROM artist WHERE title = 'Katalepsy'), 'main'),
+    ((SELECT id FROM track WHERE title = 'S.O.D.'), (SELECT id FROM artist WHERE title = 'Katalepsy'), 'main'),
+    ((SELECT id FROM track WHERE title = 'Post-Apocalyptic Segregation'), (SELECT id FROM artist WHERE title = 'Katalepsy'), 'main'),
+    ((SELECT id FROM track WHERE title = 'Carpet Wounding'), (SELECT id FROM artist WHERE title = 'Katalepsy'), 'main'),
+    ((SELECT id FROM track WHERE title = 'H. Tearing Sinew'), (SELECT id FROM artist WHERE title = 'Katalepsy'), 'main'),
+    ((SELECT id FROM track WHERE title = 'Number Of Death'), (SELECT id FROM artist WHERE title = 'Katalepsy'), 'main');
+
 
 INSERT INTO album_artist (album_id, artist_id) VALUES
     ((SELECT id FROM album WHERE title = 'Anticyclone'), (SELECT id FROM artist WHERE title = 'Inabakumori')),
@@ -90,7 +136,12 @@ INSERT INTO album_artist (album_id, artist_id) VALUES
     ((SELECT id FROM album WHERE title = 'BOOTLEG'), (SELECT id FROM artist WHERE title = 'Kenshi Yonezu')),
     ((SELECT id FROM album WHERE title = 'Your Name.'), (SELECT id FROM artist WHERE title = 'RADWIMPS')),
     ((SELECT id FROM album WHERE title = 'Official HIGE DANdism'), (SELECT id FROM artist WHERE title = 'Official HIGE DANdism')),
-    ((SELECT id FROM album WHERE title = 'Ghost City Tokyo'), (SELECT id FROM artist WHERE title = 'Ayase'));
+    ((SELECT id FROM album WHERE title = 'Ghost City Tokyo'), (SELECT id FROM artist WHERE title = 'Ayase')),
+    ((SELECT id FROM album WHERE title = 'The Eminem Show'), (SELECT id FROM artist WHERE title = 'Eminem')),
+    ((SELECT id FROM album WHERE title = 'Symphony Soldier'), (SELECT id FROM artist WHERE title = 'The Cab')),
+    ((SELECT id FROM album WHERE title = 'Everlasting Summer'), (SELECT id FROM artist WHERE title = 'Sergey Eybog')),
+    ((SELECT id FROM album WHERE title = 'Music Brings Injures'), (SELECT id FROM artist WHERE title = 'Katalepsy')),
+    ((SELECT id FROM album WHERE title = 'Triumph Of Evilution'), (SELECT id FROM artist WHERE title = 'Katalepsy'));
 
 REFRESH MATERIALIZED VIEW artist_stats;
 REFRESH MATERIALIZED VIEW album_stats;
