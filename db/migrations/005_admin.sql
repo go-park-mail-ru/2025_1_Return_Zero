596aaa3f42aeadef35cf88561088d42bc74770ef
Garoev Aleksandr <garoevae@student.bmstu.ru>
2025-05-28 11:45:30 +0300

    update migrations

--- db/migrations/005_admin.sql
+++ db/migrations/005_admin.sql
@@ -1,16 +1,20 @@
 INSERT INTO "user" (username, password_hash, email, thumbnail_url, created_at, updated_at)
-VALUES ('admin', 'AQIDBAUGBwjZy6L5y+0MZhP2MUf9TYynMMdaJmjVm766KGjOeODhDQ==', 'admin@admin.ru', '/default_avatar.png', NOW(), NOW());
+VALUES ('admin', 'AQIDBAUGBwjZy6L5y+0MZhP2MUf9TYynMMdaJmjVm766KGjOeODhDQ==', 'admin@admin.ru', '/default_avatar.png', NOW(), NOW())
+ON CONFLICT DO NOTHING;
 
 INSERT INTO "user_settings" (user_id, is_public_playlists, is_public_minutes_listened, is_public_favorite_artists, is_public_tracks_listened, is_public_favorite_tracks, is_public_artists_listened)
 VALUES (
   (SELECT id FROM "user" WHERE username = 'admin'),
   false, false, false, false, false, false
-);
+)
+ON CONFLICT DO NOTHING;
 
 ---- create above / drop below ----
 
 DELETE FROM "user_settings"
-WHERE user_id = (SELECT id FROM "user" WHERE username = 'admin');
+WHERE user_id = (SELECT id FROM "user" WHERE username = 'admin')
+ON CONFLICT DO NOTHING;
 
 DELETE FROM "user"
-WHERE username = 'admin';
+WHERE username = 'admin'
+ON CONFLICT DO NOTHING;
