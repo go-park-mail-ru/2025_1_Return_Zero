d79e0dd595cf9aa001e6472098c7b1c8532848d5
Garoev Aleksandr <garoevae@student.bmstu.ru>
2025-05-28 14:30:20 +0300

    increase limits

--- microservices/track/cmd/main.go
+++ microservices/track/cmd/main.go
@@ -60,8 +60,8 @@ func main() {
 
 	server := grpc.NewServer(
 		grpc.UnaryInterceptor(accessInterceptor.UnaryServerInterceptor()),
-		grpc.MaxRecvMsgSize(50*1024*1024), // 50 MB
-		grpc.MaxSendMsgSize(50*1024*1024), // 50 MB
+		grpc.MaxRecvMsgSize(500*1024*1024), // 500 MB
+		grpc.MaxSendMsgSize(500*1024*1024), // 500 MB
 	)
 
 	go func() {
